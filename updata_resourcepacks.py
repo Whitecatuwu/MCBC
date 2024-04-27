92b9b7a4a8619c85dbd178132f89cbd9102e3c2e
uwuWhitecat <aisbn3710@gmail.com>
2024-04-27 14:30:01 +0800

    updata_resourcepacks.py

--- updata_resourcepacks.py
+++ updata_resourcepacks.py
@@ -1,7 +1,6 @@
 from shutil import copy2,copytree,rmtree
 from os import scandir,remove,path
 from time import time as currenttime
-import fnmatch
 
 start_time = currenttime()
 current = path.dirname(path.abspath(__file__))
@@ -16,10 +15,16 @@ def Cyan(skk): return "\033[96m{}\033[00m".format(skk) #skip
 def White(skk): return "\033[97m{}\033[00m".format(skk) 
 
 
-def filtercopy(old=True):
-    #filter old file.
-    def _filter(src, dst):
-        if old == False or path.exists(dst) == False or path.getmtime(src) > path.getmtime(dst): 
+def is_valid_pathname(path):
+    from re import match
+    pattern = r'^[a-zA-Z]:\\(?:[a-zA-Z0-9-_ ]+\\)*[a-zA-Z0-9-_ ]+\.\w+$'
+    return match(pattern, path) is not None
+
+def filtercopy(old=True) -> callable:
+    #ignore old file when old == True.
+    def _filter(src, dst) -> None:
+        cancopy:bool = (old == False) or (path.exists(dst) == False) or (path.getmtime(src) > path.getmtime(dst))
+        if cancopy: 
             try: 
                 copy2(src, dst)
             except FileNotFoundError as e: 
@@ -30,17 +35,18 @@ def filtercopy(old=True):
                 print(Green(f"Update: {dst}"))
     return _filter
 
-def ignorepath(pathlist:dict,src,dst,purge=False):
-    def _ignore(path_src,names):
-        #fnmatch.filter(names, pattern)
+def ignorepath(pathlist:dict,src,dst,purge=False) -> callable:
+    def _ignore(path_src,names) -> set:
+        from fnmatch import filter as fn_filter
+        #fn_filter(names, pattern)
         delete_list = [x for x in names if path.join(path_src,x) in [src+p for p in pathlist["D"]]]
         for fi in filter(lambda x:not path.isabs(x),pathlist["D"]):
-            delete_list.extend(fnmatch.filter(names, fi))
+            delete_list.extend(fn_filter(names, fi))
         delete_list = set(delete_list)
 
         modify_list = [x for x in names if path.join(path_src,x) in [src+p for p in pathlist["M"]]]
         for fi in filter(lambda x:not path.isabs(x),pathlist["M"]):
-            modify_list.extend(fnmatch.filter(names, fi))
+            modify_list.extend(fn_filter(names, fi))
         modify_list = set(modify_list)
 
         ignore_list = delete_list | modify_list
@@ -49,7 +55,8 @@ def ignorepath(pathlist:dict,src,dst,purge=False):
             path_dst = path_src.replace(src,dst)
             if path.exists(path_dst):
                 for d in list(scandir(path_dst)):
-                    if (d.name in delete_list) or ( (d.name not in names) and (path.join(path_dst,d.name) not in [dst+p for p in pathlist["M"]]) ):
+                    can_delete:bool = (d.name in delete_list) or ( (d.name not in names) and (path.join(path_dst,d.name) not in [dst+p for p in pathlist["M"]]) )
+                    if  can_delete:
                         try:
                             if path.isdir(d.path): 
                                 rmtree(d.path)
@@ -67,7 +74,7 @@ def ignorepath(pathlist:dict,src,dst,purge=False):
         return ignore_list
     return _ignore
 
-def updata(pre_ver,ver):
+def updata(pre_ver:str,ver:str) -> None:
     src = current + "\\battlecats" + pre_ver
     dst = current + "\\battlecats" + ver 
     #Get paths of files that are reivsed.
@@ -112,7 +119,7 @@ def updata(pre_ver,ver):
             continue  
 
 vers = ["","_1.17.1","_1.18.2","_1.19.2","_1.19.3","_1.19.4","_1.20.1","_1.20.2","_1.20.4","_1.20.5"]
-#rescorce_ver = {"1.17.1":7,"1.18.2":8,"1.19.2":9,"1.19.3":12,"1.19.4":13,"1.20.1":15,"1.20.2":18,"1.20.4":22,"1.20.5":32}
+#resource_ver = {"1.17.1":7,"1.18.2":8,"1.19.2":9,"1.19.3":12,"1.19.4":13,"1.20.1":15,"1.20.2":18,"1.20.4":22,"1.20.5":32}
 
 try:
     for i in range(1,len(vers),1): 
