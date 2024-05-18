aaf8c555237aff73ba187f0e59e8ec3b13c22262
uwuWhitecat <aisbn3710@gmail.com>
2024-05-18 13:14:04 +0800

    py

--- updata_resourcepacks.py
+++ updata_resourcepacks.py
@@ -24,7 +24,7 @@ def filtercopy(ignore_old=True) -> callable:
     #ignore old file when old == True.
     def _filter(src, dst) -> None:
         dst_is_older:bool = (path.exists(dst) == False) or (path.getmtime(src) > path.getmtime(dst))
-        if (ignore_old == False) or dst_is_older: 
+        if (not ignore_old) or dst_is_older: 
             try: 
                 copy2(src, dst)
             except FileNotFoundError as e: 
@@ -34,6 +34,14 @@ def filtercopy(ignore_old=True) -> callable:
             else: 
                 print(Green(f"Update: {dst}"))
     return _filter
+
+def delete(pathname:str) -> None:
+    try:
+        rmtree(pathname) if path.isdir(pathname) else remove(pathname)
+    except Exception as e: 
+        print(Red(f"Delete failed: {pathname} \nBecause: {e}"))
+    else: 
+        print(Purple(f"Delete: {pathname}"))
 
 def ignorepath(pathlists:dict, namespace_src:str, namespace_dst:str, purge:bool = False) -> callable:
     def _ignore(path_src:str, names:list) -> set:
@@ -57,26 +65,14 @@ def ignorepath(pathlists:dict, namespace_src:str, namespace_dst:str, purge:bool 
                 dirname,filename = path.split(path_M)
                 if path_src == namespace_src + dirname or dirname == "":
                     modify_set.update(set(fn_filter(names, filename)))
-                    if filename not in names:
-                        add_set.add(filename)
+                    add_set.add(filename) if filename not in names else None        
 
             ignore_set = delete_set.union(modify_set)
     
-        if purge:
-            path_dst = path_src.replace(namespace_src,namespace_dst)
-            if path.exists(path_dst):
-                for d in list(scandir(path_dst)):
-                    can_delete:bool = (d.name in delete_set) or (d.name not in (set(names) | modify_set | add_set) )
-                    if can_delete:
-                        try:
-                            if path.isdir(d.path): 
-                                rmtree(d.path)
-                            else: 
-                                remove(d.path)
-                        except Exception as e: 
-                            print(Red(f"Delete failed: {d.path} \nBecause: {e}"))
-                        else: 
-                            print(Purple(f"Delete: {d.path}"))
+        if (purge == True) and path.exists(path_dst := path_src.replace(namespace_src,namespace_dst)):
+            for d in list(scandir(path_dst)):
+                can_delete:bool = (d.name in delete_set) or (d.name not in (set(names) | modify_set | add_set) )
+                delete(d.path) if can_delete else None
 
         for dele in delete_set:
             print(Grey(f"Ignore src: {path.join(path_src,dele)}"))
@@ -89,11 +85,11 @@ def ignorepath(pathlists:dict, namespace_src:str, namespace_dst:str, purge:bool 
     return _ignore
 
 def updata(pre_ver:str,ver:str) -> None:
-    src = current + "\\battlecats" + pre_ver
-    dst = current + "\\battlecats" + ver 
+    src:str = current + "\\battlecats" + pre_ver
+    dst:str = current + "\\battlecats" + ver 
     #Get paths of files that are revised.
-    modify_path = current + "\\battlecats\\vers\\" + ver[1:]
-    pathlists = {'R':[],'M':[],'D':[]}
+    modify_path:str = current + "\\battlecats\\vers\\" + ver[1:]
+    pathlists:dict = {'R':[],'M':[],'D':[]}
     #R:rename, #M:modify, D:delete
     try:
         with open(modify_path + "\\Modify.txt","r") as r:
@@ -112,17 +108,17 @@ def updata(pre_ver:str,ver:str) -> None:
     except Exception as e:
         print(Red(f"Error: {e}"))
     
-    #Copy files that not in ignore list and revise list.
+    #Copy files that not in ignore list and not in modify list.
     copytree(src+"\\assets", dst+"\\assets", dirs_exist_ok=True, ignore=ignorepath(pathlists,src,dst,purge=True), copy_function=filtercopy(ignore_old=True))
     
-    #Modify files.
+    #Copy files from path "battlecats/vers/{ver}".
     for R in pathlists["M"]:
-        s = path.join(modify_path, path.basename(R))
+        s:str = path.join(modify_path, path.basename(R))
         if path.exists(s) == False: 
             #print(Red(f"{src + R} does not exist."))
             print(Red(f"{s} does not exist."))
             continue
-        d = dst + R
+        d:str = dst + R
 
         if not path.exists(path.dirname(d)):
             print(Red(f"Updata failed: {d} \nBecause: \"{path.dirname(d)}\" does not exist"))
@@ -134,8 +130,8 @@ def updata(pre_ver:str,ver:str) -> None:
             else: 
                 print(Red(f"Updata failed: {d} \nBecause: \"{s}\" is not a valid path.")) 
 
-vers = ["","_1.17.1","_1.18.2","_1.19.2","_1.19.3","_1.19.4","_1.20.1","_1.20.2","_1.20.4","_1.20.6"]
-#resource_ver = {"1.17.1":7,"1.18.2":8,"1.19.2":9,"1.19.3":12,"1.19.4":13,"1.20.1":15,"1.20.2":18,"1.20.4":22,"1.20.6":32}
+vers = ["", "_1.17.1", "_1.18.2", "_1.19.2", "_1.19.3", "_1.19.4", "_1.20.1", "_1.20.2", "_1.20.4", "_1.20.6"]
+#resource_ver = {"1.17.1":7, "1.18.2":8, "1.19.2":9, "1.19.3":12, "1.19.4":13, "1.20.1":15, "1.20.2":18, "1.20.4":22, "1.20.6":32}
 
 try:
     #for i in range(1,2,1): 
