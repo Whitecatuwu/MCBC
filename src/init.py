dd3ff7eada36d930335e63235dd02fdfe1d32e9a
uwuWhitecat <aisbn3710@gmail.com>
2026-01-21 19:26:42 +0800

    feat: add configuration reading functionality and refactor init to use it

--- src/init.py
+++ src/init.py
@@ -2,7 +2,7 @@ from src.PackUpdate import PackUpdate
 from src.ResPack import ResPack
 from src.gui.ansi import Strong
 from os import path as os_path
-import configparser
+import src.config
 
 PACK_PATH = "PackPath"
 OPERATION_PATH = "OperationsPath"
@@ -11,9 +11,7 @@ OLDER_VERS = ["1.16.5", "1.16.1", "1.14.4", "1.12.2", "1.10.2", "1.8.9"]
 
 def init(base_path: str) -> callable:
 
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-    __check_config(config)
+    config = src.config.read_config()
 
     versions: list[str] = config.options(OPERATION_PATH)
 
@@ -47,20 +45,3 @@ def init(base_path: str) -> callable:
             PackUpdate(ver_res_packs[i - 1], ver_res_packs[i]).update()
 
     return update_older, update_newer
-
-
-def __check_config(config: configparser.ConfigParser) -> None:
-
-    if not config.has_section(PACK_PATH):
-        raise ValueError(f"Section '{PACK_PATH}' not found")
-    if not config.has_section(OPERATION_PATH):
-        raise ValueError(f"Section '{OPERATION_PATH}' not found")
-
-    if not config.has_option(PACK_PATH, "core"):
-        raise ValueError(f"Option 'core' not found in section '{PACK_PATH}' ")
-
-    for ver in config.options(PACK_PATH):
-        if ver == "core":
-            continue
-        if not config.has_option(OPERATION_PATH, ver):
-            raise ValueError(f"Option '{ver}' not found in section '{OPERATION_PATH}' ")
